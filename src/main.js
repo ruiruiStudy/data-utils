e87e5f0dd263d79fe1dfeb4e0d1669f311324e40
yanrui <yanrui7037@dingtalk.com>
2024-01-11 18:40:02 +0800

    引入中文

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 
 app.mount('#app')
