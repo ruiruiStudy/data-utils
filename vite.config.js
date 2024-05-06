bc367846185741caefa4e6b821ad600317058096
yanrui <yanrui7037@dingtalk.com>
2024-05-06 10:20:26 +0800

    github配置build

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { setupBuild } from "./build";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/data-utils/',
-  build: {
-    outDir: "docs"
-  },
+  build: setupBuild(),
   plugins: [vue()],
   resolve: {
     alias: {
