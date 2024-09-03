113f911195a6e47be353297928a92c1bd36e30a8
yanrui <yanrui7037@dingtalk.com>
2024-09-03 16:16:29 +0800

    修改配置33

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { setupBuild } from "./build";
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: '/',
-    build: setupBuild(),
+    base: './',
     plugins: [vue()],
     resolve: {
       alias: {
